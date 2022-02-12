9bf836026282797e0438802fd060c21124ddd6c7
Lynden Lim <lyndenlim15@gmail.com>
2022-02-11 22:43:42 -0500

    Added light/dark mode and set routing between home and favorites

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { Route } from "react-router-dom";
-import Header from "./Header";
+import { Route, Switch } from "react-router-dom";
+import Navbar from "./Navbar";
 import Favorites from "./Favorites"
 import LetterContainer from "./LetterContainer";
 
 function App() {
   return (
     <div>
-      <Route exact path="/">
-        <Header />
-        <LetterContainer />
-      </Route>
-      <Route exact path="/favorites">
-        <Favorites />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Navbar />
+          <LetterContainer />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+      </Switch>
     </div>
   );
 }
